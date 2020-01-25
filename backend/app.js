4cb77057c99d9dd1a2f9569b8eb02e75c2df9253
Lucas Lessa <lucaslessa14@hotmail.com>
2020-01-24 22:28:25 -0300

    Aplicação finalizada

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const restful = require('node-restful');
-const app = express();
-const mongoose = restful.mongoose;
+const express = require('express')
+const bodyParser = require('body-parser')
+const restful = require('node-restful')
+const cors = require('cors')
+const app = express()
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://db/mydb');
+const mongoose = restful.mongoose
 
-app.get('/', (req, res) => {
-  res.send('Backend');
-});
+// Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
 
+// Middlewares
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(cors())
+
+// ODM
+const Client = restful.model('Client', {
+  name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(app, '/clients')
+
+// Start server
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!')
 });
